c063a052cf7bce8fbe60d06a541bbafe35705ac3
Andrei Suprun <andrei.suprun.by@gmail.com>
2021-04-12 15:51:27 +0300

    Initial commit

--- config/src/main/java/com/epam/esm/CustomErrorMessageConfiguration.java
+++ config/src/main/java/com/epam/esm/CustomErrorMessageConfiguration.java
@@ -5,10 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-<<<<<<< HEAD
-=======
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
->>>>>>> Initial commit
 
 @Configuration
 public class CustomErrorMessageConfiguration {
@@ -27,12 +24,9 @@ public class CustomErrorMessageConfiguration {
         bean.setValidationMessageSource(messageSource());
         return bean;
     }
-<<<<<<< HEAD
-=======
 
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
     }
->>>>>>> Initial commit
 }
--- controller/src/main/java/com/epam/esm/GiftCertificatesController.java
+++ controller/src/main/java/com/epam/esm/GiftCertificatesController.java
@@ -2,25 +2,20 @@ package com.epam.esm;
 
 import com.epam.esm.entity.GiftCertificate;
 import com.epam.esm.exceptions.NotFoundException;
-<<<<<<< HEAD
-=======
+
 import com.epam.esm.exceptions.UnsupportedPatchOperationException;
->>>>>>> Initial commit
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-<<<<<<< HEAD
-import java.util.List;
+
 import java.util.Map;
-=======
 import javax.validation.constraints.Min;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
->>>>>>> Initial commit
+
 
 @RestController
 @Validated
@@ -45,29 +40,16 @@ public class GiftCertificatesController {
     }
 
     @GetMapping("/gift_certificates/{id}")
-<<<<<<< HEAD
-    GiftCertificate one(@PathVariable Long id) {
-=======
     GiftCertificate one(@PathVariable @Min(value = 1, message = "{id.minvalue}") Long id) {
->>>>>>> Initial commit
         return giftCertificatesService.find(id).orElseThrow(() -> new NotFoundException("certificate.notfound", id));
     }
 
     @DeleteMapping("/gift_certificates/{id}")
-<<<<<<< HEAD
-    void delete(@PathVariable Long id) {
-=======
     void delete(@PathVariable @Min(value = 1, message = "{id.minvalue}") Long id) {
->>>>>>> Initial commit
         giftCertificatesService.delete(id);
     }
 
     @PutMapping("/gift_certificates/{id}")
-<<<<<<< HEAD
-    GiftCertificate update(@RequestBody Map<String, String> requestMap, @PathVariable Long id){
-        if (requestMap.containsKey(CREATE_DATE) || requestMap.containsKey(CREATE))
-        giftCertificatesService.update(requestMap, id).orElseThrow(() -> new NotFoundException("certificate.notfound", id));
-=======
     GiftCertificate update(@RequestBody Map<String, String> requestMap, @PathVariable @Min(value = 1, message = "{id.minvalue}") Long id){
         if (requestMap.containsKey(CREATE_DATE) || requestMap.containsKey(LAST_UPDATE_DATE)){
             Set<String> fields = new HashSet<>();
@@ -76,6 +58,5 @@ public class GiftCertificatesController {
             throw new UnsupportedPatchOperationException("certificate.fields.notupdatable", fields);
         }
         return giftCertificatesService.update(requestMap, id).orElseThrow(() -> new NotFoundException("certificate.notfound", id));
->>>>>>> Initial commit
     }
 }
