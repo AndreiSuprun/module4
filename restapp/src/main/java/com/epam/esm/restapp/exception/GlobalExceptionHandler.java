ac8c714d68c1a77ab4a8b434650fdb5dcc442815
Andrei Suprun <andrei.suprun.by@gmail.com>
2021-06-09 10:54:20 +0300

    Initial commit

--- restapp/src/main/java/com/epam/esm/restapp/exception/GlobalExceptionHandler.java
+++ restapp/src/main/java/com/epam/esm/restapp/exception/GlobalExceptionHandler.java
@@ -53,7 +53,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         CustomErrorResponse apiResponse = new CustomErrorResponse();
         apiResponse.setErrorCode(ErrorCode.INTERNAL_ERROR.getCode().toString());
         apiResponse.setErrorMessage(messageSource.getMessage(ErrorCode.INTERNAL_ERROR.getMessageCode(),
-                new Object[] {ex.getCause()}, Locale.getDefault()));
+                new Object[] {}, Locale.getDefault()));
         return new ResponseEntity<>(apiResponse, ErrorCode.INTERNAL_ERROR.getHttpStatus());
     }
 
