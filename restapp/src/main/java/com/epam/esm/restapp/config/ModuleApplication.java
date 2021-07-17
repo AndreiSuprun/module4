2621d8150e1378c86233a23456420a41f56675f3
AndreiSuprun <suprun-an@mail.ru>
2021-07-17 16:16:16 +0300

    i

--- restapp/src/main/java/com/epam/esm/restapp/config/ModuleApplication.java
+++ restapp/src/main/java/com/epam/esm/restapp/config/ModuleApplication.java
@@ -1,20 +1,16 @@
 package com.epam.esm.restapp.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.epam.esm.entity"})
 @EnableJpaRepositories("com.epam.esm.dao")
 @EnableJpaAuditing
-public class ModuleApplication {
+public class ModuleApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(ModuleApplication.class, args);
